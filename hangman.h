01e08d2e04e354645b4046d8d25fadc5ca8e8af5
pyerk <fortniteitlive@gmail.com>
2026-03-16 14:52:43 +0300

    Update hangman.h

--- hangman.h
+++ hangman.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <vector>
+#include <windows.h>
 
 struct WordEntry {
     std::string word;
@@ -10,6 +11,9 @@ struct WordEntry {
 };
 
 
+enum Color { RED = 12, GREEN = 10, BLUE = 9, YELLOW = 14, WHITE = 15, CYAN = 11 };
+
+void setColor(int color);
 void drawGallows(int errors);
 void printStatus(const std::string& guessed, int tries, int hints, const std::vector<char>& used);
 std::vector<WordEntry> getLibrary();
